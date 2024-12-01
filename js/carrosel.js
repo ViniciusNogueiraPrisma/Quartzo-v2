62fc4a56d666df2105150bed70280d2f6ef1166b
vinicius nogueira <v.francisco@riprisma.com>
2024-12-01 18:30:04 -0300

    alt

--- js/carrosel.js
+++ js/carrosel.js
@@ -17,3 +17,27 @@ var swiper = new Swiper(".mySwiper-fundos", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var swiper = new Swiper(".mySwiper-equipe-v", {
+  spaceBetween: 24,
+  slidesPerView: "auto",
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySwiper-cases", {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 24,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
